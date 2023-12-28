ba7d8b5fe98cc77c6a33f0673f51df48db2d9498
Rajesh <rajesh@Admins-MacBook-Pro.local>
2023-12-28 15:13:57 +0530

    cancel func

--- index.js
+++ index.js
@@ -64,6 +64,10 @@ app.get("/paid", (req, res) => {
   res.render("paid");
 });
 
+app.get("/canceled", (req, res) => {
+  res.render("canceled");
+});
+
 app.get("/", (req, res) => {
   res.render("base", {
     title: "payment alert",
