f9f5caaf04b1605d9c28610b6d5f530442ed06f6
Rajesh <rajesh@Admins-MacBook-Pro.local>
2024-01-02 15:16:20 +0530

    polling only env updated

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 require("dotenv").config();
 
 const serverURl = process.env.REACT_APP_SOCKET_API;
-const socketURL = `${serverURl}/socket`;
-const pollingURL = `${serverURl}/polling`;
+const pollingSite = `${serverURl}/polling`;
 
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.set("view engine", "ejs");
+if (pollingSite) {
+  const express = require("express");
+  const app = express();
+  const cors = require("cors");
+  const bodyParser = require("body-parser");
 
-// const express = require("express");
-// const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
-// const cors = require("cors");
-// app.use(cors());
-const server = http.createServer(app);
-
-const receivedPaymentAlerts = [];
-const receivedPaymentAlertsFromSocket = [];
-const confirmationstatus = {};
-
-if (pollingURL) {
   app.use(
     cors({
       origin: "*",
     })
   );
+
+  const receivedPaymentAlerts = [];
+  const confirmationstatus = {};
+
+  app.use(bodyParser.json());
+
+  app.set("view engine", "ejs");
 
   app.post("/fromPaymentAlert", (req, res) => {
     //   console.log(req.body);
@@ -70,82 +62,22 @@ if (pollingURL) {
     }
   });
 
+  app.get("/paid", (req, res) => {
+    res.render("paid");
+  });
+
+  app.get("/canceled", (req, res) => {
+    res.render("canceled");
+  });
+
+  app.get("/", (req, res) => {
+    res.render("base", {
+      title: "payment alert",
+      AlertValue: receivedPaymentAlerts,
+    });
+  });
+
   app.listen(8080, () => {
     console.log("server running on port 8080");
   });
-} else if (socketURL) {
-  const io = new Server(server, {
-    cors: {
-      origin: "*",
-    },
-  });
-
-  var val = 0;
-  const socketRooms = new Map();
-
-  io.on("connection", (socket) => {
-    console.log(`user connected: ${val++} , ${socket.id}`);
-
-    socket.on("paymentPageConnected", (data) => {
-      let socketId;
-      const room = data.NewReceiver.tabId;
-      console.log(data.NewReceiver);
-      socketRooms.set(socket.id, room);
-      for (const [key, value] of socketRooms.entries()) {
-        if (value === room) {
-          socketId = room;
-          break;
-        }
-      }
-      socket.join(room);
-      if (data.connected) {
-        io.emit("paymentConfirmAlert", {
-          receivedValue: data.NewReceiver,
-          UniqueId: room,
-          socketRoom: socketId,
-        });
-      }
-    });
-
-    socket.on("join_success_room", (data) => {
-      const socketID = data.SocketRoom;
-      socket.join(socketID);
-      console.log("room joined from success page", socketID);
-    });
-
-    socket.on("clicked", (data) => {
-      const roomName = data.SocketRoom;
-      console.log("tab id", data.tabId);
-      console.log(`payment confirmed ${roomName}`);
-      if (data.clicked) {
-        io.to(data.tabId).emit("success", true); // Emit success to specific tabId
-      }
-    });
-
-    socket.on("canceled", (data) => {
-      if (data.cancel) {
-        io.to(data.tabId).emit("failed", true);
-      }
-    });
-  });
-
-  server.listen(3009, () => {
-    console.log("server running on 3004");
-  });
 }
-
-app.get("/", (req, res) => {
-  res.render("base", {
-    title: "payment alert",
-    AlertValue: receivedPaymentAlerts,
-    AlertValue2: receivedPaymentAlertsFromSocket,
-  });
-});
-
-app.get("/paid", (req, res) => {
-  res.render("paid");
-});
-
-app.get("/canceled", (req, res) => {
-  res.render("canceled");
-});
