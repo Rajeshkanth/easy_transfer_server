696991c8df043dec4cd0b29566f0682851180b94
Rajesh <rajesh@Admins-MacBook-Pro.local>
2024-01-03 13:05:50 +0530

    socket updated

--- index.js
+++ index.js
@@ -103,7 +103,9 @@ io.on("connection", (socket) => {
     let socketId;
     const room = data.NewReceiver.tabId;
     console.log(data.NewReceiver);
+    data.NewReceiver.socketRoom = socketId;
     receivedPaymentAlerts.push(data.NewReceiver);
+
     socketRooms.set(socket.id, room);
     for (const [key, value] of socketRooms.entries()) {
       if (value === room) {
