5cdad78d790cfd3e140c154c278d63712c9e4f11
Rajesh <rajesh@Admins-MacBook-Pro.local>
2024-01-03 11:19:41 +0530

    socket added

--- index.js
+++ index.js
@@ -136,7 +136,6 @@ io.on("connection", (socket) => {
     );
 
     if (itemIndex !== -1) {
-      // Remove the item from the array using splice
       receivedPaymentAlerts.splice(itemIndex, 1);
     }
 
@@ -146,6 +145,13 @@ io.on("connection", (socket) => {
   });
 
   socket.on("canceled", (data) => {
+    const itemIndex = receivedPaymentAlerts.findIndex(
+      (item) => item.tabId === data.tabId
+    );
+
+    if (itemIndex !== -1) {
+      receivedPaymentAlerts.splice(itemIndex, 1);
+    }
     if (data.cancel) {
       io.to(data.tabId).emit("failed", true);
     }
