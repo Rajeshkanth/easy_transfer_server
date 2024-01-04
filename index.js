4eedde7b21e01442b45d960dd6527b96f2db69da
Rajesh <rajesh@Admins-MacBook-Pro.local>
2024-01-04 13:14:08 +0530

    env config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const serverURl = process.env.REACT_APP_SOCKET_API;
 // const pollingSite = `${serverURl}/polling`;
-// const socketStie=`${serverURl}/socket`
+// const socketStie = process.env.CONNECTION_METHOD;
 
 const express = require("express");
 const app = express();
@@ -17,144 +17,153 @@ app.use(
 
 const receivedPaymentAlerts = [];
 const confirmationstatus = {};
+const port = process.env.PORT;
 
-app.use(bodyParser.json());
+if (process.env.CONNECTION_METHOD !== "socket") {
+  app.use(bodyParser.json());
 
-app.set("view engine", "ejs");
+  app.set("view engine", "ejs");
 
-app.post("/fromPaymentAlert", (req, res) => {
-  //   console.log(req.body);
-  newRequest = req.body.data;
-  receivedPaymentAlerts.push(newRequest);
-  res.status(200).send("received successfully");
-});
-
-app.post("/confirm/:tabId", (req, res) => {
-  console.log(req.params.tabId);
-  const tabId = req.params.tabId;
-  // const status = confirmationstatus[tabId];
-  const action = req.body.Action;
-  //   console.log(status);
-  if (action === "confirm") {
-    // confirmed = true;
-    confirmationstatus[tabId] = "confirm";
-    console.log("confirmed");
-    receivedPaymentAlerts.splice(req.body.index, 1);
-    res.status(200).send();
-  } else if (action === "cancel") {
-    // canceled = true;
-    confirmationstatus[tabId] = "cancel";
-    console.log("canceled");
-    receivedPaymentAlerts.splice(req.body.index, 1);
-    res.status(201).send();
-  }
-});
-app.post("/success/:tabId", (req, res) => {
-  const tabId = req.params.tabId;
-
-  if (confirmationstatus[tabId] === "confirm") {
-    // confirmed = false;
-    delete confirmationstatus[tabId];
-    console.log("/success :", tabId);
-    res.status(200).send("confirmed");
-  } else if (confirmationstatus[tabId] === "cancel") {
-    delete confirmationstatus[tabId];
-    console.log("canceled :", tabId);
-    res.status(201).send();
-  }
-});
-
-app.get("/paid", (req, res) => {
-  res.render("paid");
-});
-
-app.get("/canceled", (req, res) => {
-  res.render("canceled");
-});
-
-app.get("/", (req, res) => {
-  res.render("base", {
-    title: "payment alert",
-    AlertValue: receivedPaymentAlerts,
+  app.post("/fromPaymentAlert", (req, res) => {
+    //   console.log(req.body);
+    newRequest = req.body.data;
+    receivedPaymentAlerts.push(newRequest);
+    res.status(200).send("received successfully");
   });
-});
 
-// app.listen(8080, () => {
-//   console.log("server running on port 8080");
-// });
+  app.post("/confirm/:tabId", (req, res) => {
+    console.log(req.params.tabId);
+    const tabId = req.params.tabId;
+    // const status = confirmationstatus[tabId];
+    const action = req.body.Action;
+    //   console.log(status);
+    if (action === "confirm") {
+      // confirmed = true;
+      confirmationstatus[tabId] = "confirm";
+      console.log("confirmed");
+      receivedPaymentAlerts.splice(req.body.index, 1);
+      res.status(200).send();
+    } else if (action === "cancel") {
+      // canceled = true;
+      confirmationstatus[tabId] = "cancel";
+      console.log("canceled");
+      receivedPaymentAlerts.splice(req.body.index, 1);
+      res.status(201).send();
+    }
+  });
+  app.post("/success/:tabId", (req, res) => {
+    const tabId = req.params.tabId;
 
-const http = require("http");
-const { Server } = require("socket.io");
+    if (confirmationstatus[tabId] === "confirm") {
+      // confirmed = false;
+      delete confirmationstatus[tabId];
+      console.log("/success :", tabId);
+      res.status(200).send("confirmed");
+    } else if (confirmationstatus[tabId] === "cancel") {
+      delete confirmationstatus[tabId];
+      console.log("canceled :", tabId);
+      res.status(201).send();
+    }
+  });
 
-app.use(cors());
-const server = http.createServer(app);
+  app.get("/paid", (req, res) => {
+    res.render("paid");
+  });
 
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
+  app.get("/canceled", (req, res) => {
+    res.render("canceled");
+  });
 
-var val = 0;
-const socketRooms = new Map();
+  app.get("/", (req, res) => {
+    res.render("base", {
+      title: "payment alert",
+      AlertValue: receivedPaymentAlerts,
+    });
+  });
 
-io.on("connection", (socket) => {
-  console.log(`user connected: ${val++} , ${socket.id}`);
+  app.listen(port, () => {
+    console.log("server running on port 8080");
+  });
+}
 
-  socket.on("paymentPageConnected", (data) => {
-    let socketId;
-    const room = data.NewReceiver.tabId;
-    console.log(data.NewReceiver);
-    data.NewReceiver.socketRoom = socketId;
-    receivedPaymentAlerts.push(data.NewReceiver);
+if (process.env.CONNECTION_METHOD === "socket") {
+  const http = require("http");
+  const { Server } = require("socket.io");
 
-    socketRooms.set(socket.id, room);
-    for (const [key, value] of socketRooms.entries()) {
-      if (value === room) {
-        socketId = room;
-        break;
+  app.use(cors());
+  const server = http.createServer(app);
+
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  var val = 0;
+  const socketRooms = new Map();
+
+  io.on("connection", (socket) => {
+    console.log(`user connected: ${val++} , ${socket.id}`);
+
+    io.emit("connection_type", {
+      type: "socket",
+    });
+
+    socket.on("paymentPageConnected", (data) => {
+      let socketId;
+      const room = data.NewReceiver.tabId;
+      console.log(data.NewReceiver);
+      data.NewReceiver.socketRoom = socketId;
+      receivedPaymentAlerts.push(data.NewReceiver);
+
+      socketRooms.set(socket.id, room);
+      for (const [key, value] of socketRooms.entries()) {
+        if (value === room) {
+          socketId = room;
+          break;
+        }
       }
-    }
-    socket.join(room);
+      socket.join(room);
+    });
+
+    socket.on("join_success_room", (data) => {
+      const socketID = data.SocketRoom;
+      socket.join(socketID);
+      console.log("room joined from success page", socketID);
+    });
+
+    socket.on("clicked", (data) => {
+      console.log("tab id", data.tabId);
+      console.log(`payment confirmed by socket to ${data.tabId}`);
+      const itemIndex = receivedPaymentAlerts.findIndex(
+        (item) => item.tabId === data.tabId
+      );
+
+      if (itemIndex !== -1) {
+        receivedPaymentAlerts.splice(itemIndex, 1);
+      }
+
+      if (data.clicked) {
+        io.to(data.tabId).emit("success", true); // Emit success to specific tabId
+      }
+    });
+
+    socket.on("canceled", (data) => {
+      console.log(`payment canceled by socket ${data.tabId}`);
+      const itemIndex = receivedPaymentAlerts.findIndex(
+        (item) => item.tabId === data.tabId
+      );
+
+      if (itemIndex !== -1) {
+        receivedPaymentAlerts.splice(itemIndex, 1);
+      }
+      if (data.cancel) {
+        io.to(data.tabId).emit("failed", true);
+      }
+    });
   });
-
-  socket.on("join_success_room", (data) => {
-    const socketID = data.SocketRoom;
-    socket.join(socketID);
-    console.log("room joined from success page", socketID);
+  const port = 3010;
+  server.listen(port, () => {
+    console.log("server running on ", port);
   });
-
-  socket.on("clicked", (data) => {
-    console.log("tab id", data.tabId);
-    console.log(`payment confirmed by socket to ${data.tabId}`);
-    const itemIndex = receivedPaymentAlerts.findIndex(
-      (item) => item.tabId === data.tabId
-    );
-
-    if (itemIndex !== -1) {
-      receivedPaymentAlerts.splice(itemIndex, 1);
-    }
-
-    if (data.clicked) {
-      io.to(data.tabId).emit("success", true); // Emit success to specific tabId
-    }
-  });
-
-  socket.on("canceled", (data) => {
-    console.log(`payment canceled by socket ${data.tabId}`);
-    const itemIndex = receivedPaymentAlerts.findIndex(
-      (item) => item.tabId === data.tabId
-    );
-
-    if (itemIndex !== -1) {
-      receivedPaymentAlerts.splice(itemIndex, 1);
-    }
-    if (data.cancel) {
-      io.to(data.tabId).emit("failed", true);
-    }
-  });
-});
-
-server.listen(8080, () => {
-  console.log("server running on 3004");
-});
+}
