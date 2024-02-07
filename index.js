c5576ac757e3afb48b5865dc5be35f64b48c3b0d
Rajesh <rajesh@Rajesh.local>
2024-02-07 16:24:57 +0530

    checking

--- index.js
+++ index.js
@@ -524,7 +524,7 @@ if (process.env.CONNECTION_METHOD === "socket") {
 
         if (userFound) {
           // Check if the beneficiary with the same account number already exists
-          const existingBeneficiary = userFound.savedAccounts.some(
+          const existingBeneficiary = userFound.savedAccounts.find(
             (account) => {
               account.accNum === SavedAccNum;
             }
